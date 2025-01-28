da4a67a7a5103383d543918c7e8a2146d2abae93
devfelipecesar <devfelipecesar@gmail.com>
2025-01-28 13:14:20 -0300

    change code and function increment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-print('''
+def exibir_nome_do_programa():
+
+    print('''
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -10,13 +12,58 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''')
 
-def menu_opcao():
+def exibir_opcao():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    #opcao_escolhida = int(opcao_escolhida)
+
+    # TESTE DE VALOR ARMAZENADO NA VARIAVEL
+    # print(opcao_escolhida == 1)
+    # print(type(opcao_escolhida))
+    # print(type(1))
+    print(f'Você escolheu a opcao: {opcao_escolhida}')
+
+    # if opcao_escolhida == 1:
+    #     print('Cadastrar restaurante')
+    # elif opcao_escolhida == 2:
+    #     print('Listar restaurantes')
+    # elif opcao_escolhida == 3:
+    #     print('Ativar restaurante')
+    # else:
+    #     finalizar_app()
+    # #def encerrar():
+
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurante')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opcao invalida')
+
+
+def finalizar_app():
+    os.system('cls')
+    #os.system('clear') no mac
+    print('Encerrando o App\n')
+
+
+
+
 
 def main():
-    menu_opcao()
+    exibir_nome_do_programa()
+    exibir_opcao()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
